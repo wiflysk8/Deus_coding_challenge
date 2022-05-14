db171147da52a6e53a1ed6c1f17169f604301cb5
Luis Martínez <wifly.sk8@gmail.com>
2022-05-14 02:33:51 +0200

    responsive media query

--- src/Pages/BeerDetail/BeerDetail.jsx
+++ src/Pages/BeerDetail/BeerDetail.jsx
@@ -23,10 +23,15 @@ const BeerDetail = () => {
       <section className="c-beerDetail">
         <h1 className="c-beerDetail__name">{beerDetail.name}</h1>
         <img className="c-beerDetail__img" src={beerDetail.image_url} alt="" />
-        <p className="c-beerDetail__description">{beerDetail.description}</p>
+        <h3>Food pairing:</h3>
+        <div className="c-beerDetail__food">
+          {beerDetail.food_pairing.map((pairing) => (
+            <p className="c-beerDetail__description">{pairing}</p>
+          ))}
+        </div>
         <Link to="/">
           <button class="cta">
-            <span class="hover-underline-animation"> Back to Gallery </span>
+            <span class="hover-underline-animation"> Go to Gallery </span>
             <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
               <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
             </svg>
